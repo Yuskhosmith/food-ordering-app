ed49208776e1b56712036aff83ea953fdb027393
Yukhosmith <yuskhosmith1@gmail.com>
2023-06-05 23:03:15 +0100

    modal done

--- src/components/Layout/HeaderCartButton/headercardbutton.js
+++ src/components/Layout/HeaderCartButton/headercardbutton.js
@@ -1,13 +1,22 @@
+import { useContext } from 'react';
+
 import CartIcon from '../../Cart/cartIcon/CartIcon';
 import classes from './headercardbutton.module.css';
+import CartContext from '../../../store/cart-context';
+
 const HeaderCardButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const numberOfCartItems = ctx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0);
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
